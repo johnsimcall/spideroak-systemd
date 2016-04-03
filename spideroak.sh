a0d01170ebd252564b760a6a80caf3744e3cba18
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2016-04-03 20:54:55 +0100

    Minor fix regarding correct sourcing

--- spideroak.sh
+++ spideroak.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # Start/Stop script for SpiderOak to work `nicer` with per-user options
-#
-# params:
-# -> $1 = user home directoy, where the `.spideroak.rc` file is
 
-echo "Checking if $1/.spideroak.rc exists"
-if [ -f "$1/.spideroak.rc" ]; then
+echo "Checking if $HOME/.spideroak.rc exists"
+if [ -f "$HOME/.spideroak.rc" ]; then
     echo "Sourcing..."
-    source "$1/.spideroak.rc"
+    source "$HOME/.spideroak.rc"
 fi
 
 echo "Starting SpiderOak with ARGs = ${ARG:-'nothing set :('}"
